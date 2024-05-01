0cc7ca51a506e9e85a0aea09a3062ac6f047b1d0
MariiaSam <kontragent@ukr.net>
2024-05-01 17:41:45 +0300

    task_model

--- models/task.js
+++ models/task.js
@@ -2,8 +2,36 @@ import { Schema, model } from "mongoose";
 
 import handleMongooseError from "../helpers/handleMongooseError.js";
 
-const taskSchema = new Schema(
-// write the code here
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Set title for task"],
+  },
+  description: {
+    type: String,
+    required: [true, "Set description for task"],
+  },
+  priority: {
+    type: String,
+    enum: ["without", "low", "medium", "high"],
+    required: [true, "Set priority for task"],
+    default: "without",
+  },
+  deadline: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: 'true'
+},
+  column: {
+    type: Schema.Types.ObjectId,
+    ref: 'column',
+    required: 'true'
+},
+},   { versionKey: false, timestamps: true }
 );
 
 taskSchema.post("save", handleMongooseError);
