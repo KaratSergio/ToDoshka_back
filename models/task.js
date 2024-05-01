f382191e2e10b302cda50125105e5a655baf1cc2
MariiaSam <kontragent@ukr.net>
2024-05-01 22:41:50 +0300

    Column_Model

--- models/task.js
+++ models/task.js
@@ -2,36 +2,38 @@ import { Schema, model } from "mongoose";
 
 import handleMongooseError from "../helpers/handleMongooseError.js";
 
-const taskSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, "Set title for task"],
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Set title for task"],
+    },
+    description: {
+      type: String,
+      required: [true, "Set description for task"],
+    },
+    priority: {
+      type: String,
+      enum: ["without", "low", "medium", "high"],
+      required: [true, "Set priority for task"],
+      default: "without",
+    },
+    deadline: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: "true",
+    },
+    column: {
+      type: Schema.Types.ObjectId,
+      ref: "column",
+      required: "true",
+    },
   },
-  description: {
-    type: String,
-    required: [true, "Set description for task"],
-  },
-  priority: {
-    type: String,
-    enum: ["without", "low", "medium", "high"],
-    required: [true, "Set priority for task"],
-    default: "without",
-  },
-  deadline: {
-    type: Date,
-    default: Date.now,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-    required: 'true'
-},
-  column: {
-    type: Schema.Types.ObjectId,
-    ref: 'column',
-    required: 'true'
-},
-},   { versionKey: false, timestamps: true }
+  { versionKey: false, timestamps: true }
 );
 
 taskSchema.post("save", handleMongooseError);
