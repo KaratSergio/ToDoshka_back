8afdc587b397c8120800b16ddcd92c7c9c4c09a4
MariiaSam <kontragent@ukr.net>
2024-05-02 14:03:32 +0300

    add

--- routes/routerTasks.js
+++ routes/routerTasks.js
@@ -3,14 +3,19 @@ import express from "express";
 import isValidId from "../middlewares/isValidId.js";
 import isAuthorized from "../middlewares/isAuthorized.js";
 
+import validateBody from "../decorators/validateBody.js";
+
 import addTask from "../controllers/task/addTask.js";
 import updateTask from "../controllers/task/updateTask.js";
 import deleteTask from "../controllers/task/deleteTask.js";
 import transferTask from "../controllers/task/transferTask.js";
 
+import taskSchemas from "../schemas/schemaTask.js";
+
+
 const routerTasks = express.Router();
 
-routerTasks.post("/", isAuthorized, addTask);
+routerTasks.post("/", isAuthorized, validateBody(taskSchemas.addTaskSchema), addTask);
 
 routerTasks.put("/:id", isAuthorized, isValidId, updateTask);
 
