cb94cf351d0c41db6a97446affe4d2e51570094b
MariiaSam <kontragent@ukr.net>
2024-05-02 19:07:47 +0300

    task controllers

--- controllers/task/updateTask.js
+++ controllers/task/updateTask.js
@@ -1,5 +1,16 @@
-const updateTask = async (req, res) => {
-// write the code here
-};
+import HttpError from "../../helpers/HttpError.js";
+import ctrlWrapper from "../../decorators/ctrlWrapper.js";
+
+import Task from "../../models/task.js";
+
+const updateTask = ctrlWrapper(async (req, res) => {
+  const { id } = req.params;
+
+  const result = await Task.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, `Task with id=${id} not found`);
+  }
+  res.json(result);
+});
 
 export default updateTask;
