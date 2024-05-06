9e158a77f1b8cf9a5db3fbb2bae186245c940b27
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-05-06 16:34:28 +0300

    Update register controller
    add accessToken

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -10,19 +10,30 @@ const register = async (req, res, next) => {
   const { value, error } = authSchema.registerSchema.validate(req.body, {
     abortEarly: false,
   });
+
   if (error) {
-    const httpError = BadRequestError(error, req, res);
-    return next(httpError);
+    return next(new BadRequestError("Validation failed", error.details));
   }
   const { name, email, password } = value;
 
   const userEmail = await User.findOne({ email });
   if (userEmail) {
-    return next(HttpError(409, "Email has already in use"));
+    throw new HttpError(409, "Email is already in use");
   }
 
-  const result = await User.create({ name, email, password });
-  res.status(201).json(result);
+  const newUser = await User.create({ name, email, password });
+  const accessToken = newUser.tokenAuth();
+  newUser.accessToken = accessToken;
+  await newUser.save();
+  res.status(201).json({
+    accessToken,
+    user: {
+      name: newUser.name,
+      email: newUser.email,
+      theme: newUser.theme,
+      avatarURL: newUser.avatarURL,
+    },
+  });
 };
 
 export default register;
